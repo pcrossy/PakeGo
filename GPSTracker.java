5cc37c1f31d9f8b6c066ae6930ee3192414d64ef
Pygentrix <twelli@yahoo.de>
2016-07-21 23:02:43 +0200

    Initial classes for Mocking Pokemon Go locations

---
+++ GPSTracker.java
@@ -0,0 +1,199 @@
+package pcrossy.root.pakego;
+
+
+import android.Manifest;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.provider.Settings;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
+
+/**
+ * Created by Pyrox on 21.07.2016.
+ */
+public class GPSTracker extends Service implements LocationListener {
+
+    private final Context mContext;
+
+    boolean isGPSEnabled = false;
+
+    boolean isNetworkEnabled = false;
+
+    boolean canGetLocation = false;
+
+    Location location;
+    double latitude;
+    double longitude;
+
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 5;
+    private static final long MIN_TIME_BW_UPDATES = 1000 * 60 * 1;
+
+    protected LocationManager mLocationManager;
+
+    public Location getLocation() {
+        try {
+            mLocationManager = (LocationManager) mContext
+                    .getSystemService(LOCATION_SERVICE);
+
+            // getting GPS status
+            isGPSEnabled = mLocationManager
+                    .isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+            // getting network status
+            isNetworkEnabled = mLocationManager
+                    .isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+            if (!isGPSEnabled && !isNetworkEnabled) {
+                // no network provider is enabled
+            } else {
+                this.canGetLocation = true;
+                // First get location from Network Provider
+                if (isNetworkEnabled) {
+                    mLocationManager.requestLocationUpdates(
+                            LocationManager.NETWORK_PROVIDER,
+                            MIN_TIME_BW_UPDATES,
+                            MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+                    Log.d("Network", "Network");
+                    if (mLocationManager != null) {
+                        location = mLocationManager
+                                .getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                        if (location != null) {
+                            latitude = location.getLatitude();
+                            longitude = location.getLongitude();
+                        }
+                    }
+                }
+                // if GPS Enabled get lat/long using GPS Services
+                if (isGPSEnabled) {
+                    if (location == null) {
+                        mLocationManager.requestLocationUpdates(
+                                LocationManager.GPS_PROVIDER,
+                                MIN_TIME_BW_UPDATES,
+                                MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+                        Log.d("GPS Enabled", "GPS Enabled");
+                        if (mLocationManager != null) {
+                            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                                // TODO: Consider calling
+                                //    ActivityCompat#requestPermissions
+                                // here to request the missing permissions, and then overriding
+                                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                //                                          int[] grantResults)
+                                // to handle the case where the user grants the permission. See the documentation
+                                // for ActivityCompat#requestPermissions for more details.
+                                return location;
+                            }
+                            location = mLocationManager
+                                    .getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                            if (location != null) {
+                                latitude = location.getLatitude();
+                                longitude = location.getLongitude();
+                            }
+                        }
+                    }
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return location;
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+    /**
+     * Function to get latitude
+     * */
+    public double getLatitude(){
+        if(location != null){
+            latitude = location.getLatitude();
+        }
+
+        // return latitude
+        return latitude;
+    }
+
+    /**
+     * Function to get longitude
+     * */
+    public double getLongitude(){
+        if(location != null){
+            longitude = location.getLongitude();
+        }
+
+        // return longitude
+        return longitude;
+    }
+
+    private int setMockLocationSettings() {
+        int value = 1;
+        try {
+            value = Settings.Secure.getInt(getContentResolver(),
+                    Settings.Secure.ALLOW_MOCK_LOCATION);
+            Settings.Secure.putInt(getContentResolver(),
+                    Settings.Secure.ALLOW_MOCK_LOCATION, 1);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return value;
+    }
+
+    private void restoreMockLocationSettings(int restore_value) {
+        try {
+            Settings.Secure.putInt(getContentResolver(),
+                    Settings.Secure.ALLOW_MOCK_LOCATION, restore_value);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void setMockLocation(Location fakeLocation){
+    /* every time you mock location, you should use these code */
+        int value = setMockLocationSettings();//toggle ALLOW_MOCK_LOCATION on
+        try{
+            mLocationManager.setTestProviderLocation(LocationManager.GPS_PROVIDER, fakeLocation);
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        } finally {
+            restoreMockLocationSettings(value);//toggle ALLOW_MOCK_LOCATION off
+        }
+    }
+
+    public GPSTracker(Context context){
+
+        this.mContext = context;
+        getLocation();
+    }
+}
